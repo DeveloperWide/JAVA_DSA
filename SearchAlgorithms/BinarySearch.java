f8d1037a282310db603c9f1db79a5be63ca756b2
Mahesh Rana <maheshisdelta@gmail.com>
2025-11-09 22:50:56 +0530

    Binary Search Algorithms

--- SearchAlgorithms/BinarySearch.java
+++ SearchAlgorithms/BinarySearch.java
@@ -3,16 +3,16 @@ import java.util.*;
 public class BinarySearch{
 
     public static int binarySearch(int[] arr, int target){
-        int start = 0;
-        int end = arr.length - 1;
+        int start = 0; 
+        int end = arr.length - 1; 
 
         while(start <= end){
             int mid = start + (end - start) / 2; // prevents overflow
-
+            
             if(arr[mid] == target){
                 return mid;
             }
-            else if(arr[mid] < target){
+            else if(arr[mid] < target){ 
                 start = mid + 1;   // search right half
             }
             else{
@@ -24,9 +24,10 @@ public class BinarySearch{
 
 
     public static void main(String[] args) {
-        int nums[] = {30, 24, 31, 79, 35, 39};
+        int nums[] = {10, 20, 30 , 40, 50, 79, 89, 90, 125};
         int key = 79;
         
         int idx = binarySearch(nums, key);
+        System.out.println(idx);
     }
 }
