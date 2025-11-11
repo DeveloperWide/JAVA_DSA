e2ebc80e85e313e49ec931454db9667235bc9173
Mahesh Rana <maheshisdelta@gmail.com>
2025-11-11 23:16:45 +0530

    Max SubArray Sum - II (Prefix Sum)

---
+++ Arrays (Part-I)/Prefix.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class Prefix{
+
+    public static int prefixSum(int[] arr){
+        int[] prefixSum = new int[arr.length];
+        prefixSum[0] = arr[0];
+
+        int maxSum = Integer.MIN_VALUE;
+        int min_prefix_sum = 0;
+
+        for(int i=1; i<arr.length; i++){
+            prefixSum[i] +=  prefixSum[i-1] + arr[i];
+        }
+
+        for(int i=0; i<prefixSum.length; i++){
+            maxSum = Math.max(maxSum, prefixSum[i] - min_prefix_sum);
+            min_prefix_sum = Math.min(min_prefix_sum, prefixSum[i]);
+        }
+
+        return maxSum;
+    }
+
+
+    public static void main(String[] args){
+        int[] arr ={10, 20, 30, -40 , 50, -60};
+        System.out.println(prefixSum(arr));
+    }
+}
